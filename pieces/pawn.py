ba06abb9a4951e3e66681fe6cede796f43707527
James Thomson <jamestpmb@gmail.com>
2023-12-26 14:40:16 +0200

    Piece init functions
    - Added init functions to all piece classes
    - Init functions call super.__init__()

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -2,7 +2,8 @@ from pieces.piece import Piece
 
 class Pawn(Piece):
   
-  def __init__(self):
+  def __init__(self, team, position, image):
+    super().__init__(team, position, image)
     self.has_moved = False
 
   def get_available_moves(self):
--- pieces/queen.py
+++ pieces/queen.py
@@ -2,5 +2,8 @@ from pieces.piece import Piece
 
 class Queen(Piece):
 
+  def __init__(self, team, position, image):
+    super().__init__(team, position, image)
+
   def get_available_moves(self):
     return
