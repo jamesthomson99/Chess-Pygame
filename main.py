51dc62d3fda8cabd11f880e3008547c92545b70c
James Thomson <jamestpmb@gmail.com>
2023-12-26 14:20:58 +0200

    Removed window resizing
    - Removed ability to resize window (for now) to simplify implementation

--- main.py
+++ main.py
@@ -18,13 +18,8 @@ icon = pygame.image.load('images/icon_small.png')
 pygame.display.set_icon(icon)
 
 # Create the Pygame window
-board_width, board_height = BOARD_SIZE, BOARD_SIZE
-smallest_board_dimension = np.min([board_width, board_height])
-board_width, board_height = smallest_board_dimension, smallest_board_dimension
-square_size = smallest_board_dimension / GRID_SIZE
-screen = pygame.display.set_mode((board_width, board_height), pygame.RESIZABLE)
+screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
 pygame.display.set_caption("Chess")
-
 
 # Main game loop
 while True:
@@ -32,24 +27,17 @@ while True:
     if event.type == pygame.QUIT:
       pygame.quit()
       sys.exit()
-    elif event.type == pygame.VIDEORESIZE:
-      # Update window size when the window is resized
-      board_width, board_height = pygame.display.get_surface().get_size()
-      smallest_board_dimension = np.min([board_width, board_height])
-      board_width, board_height = smallest_board_dimension, smallest_board_dimension
-      square_size = smallest_board_dimension / GRID_SIZE
-      screen = pygame.display.set_mode((board_width, board_height), pygame.RESIZABLE)
 
     # Draw the chessboard
     for row in range(GRID_SIZE):
       for col in range(GRID_SIZE):
-        x = col * square_size
-        y = row * square_size
+        x = col * SQUARE_SIZE
+        y = row * SQUARE_SIZE
 
         # Alternate colors for chessboard pattern
         color = CREAM if (row + col) % 2 == 0 else BROWN
 
-        pygame.draw.rect(screen, color, (x, y, square_size, square_size))
+        pygame.draw.rect(screen, color, (x, y, SQUARE_SIZE, SQUARE_SIZE))
 
     # Update the display
     pygame.display.flip()
